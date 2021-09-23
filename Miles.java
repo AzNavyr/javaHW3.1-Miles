2374cbbd8e4be5219de3d073aecfbd54e3e7715f
Osmanov Enver <cesar-88@mail.ru>
2021-09-23 18:45:56 +0300

    Initial

---
+++ Miles.java
@@ -0,0 +1,8 @@
+public class Miles {
+    public static void main(String[] args) {
+        int price = 5281;
+        int miles = 20;
+        int totalMiles = price/miles;
+        System.out.println(totalMiles);
+    }
+}
